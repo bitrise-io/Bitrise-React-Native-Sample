983a871633e63b6903a0a148c54cadd0f6a3b155
Atanas Chanev <atanas.chanev@bitrise.io>
2022-11-22 12:33:24 +0000

    React Native

--- ios/TodoMVCTests/TodoMVCTests.m
+++ ios/TodoMVCTests/TodoMVCTests.m
@@ -5,7 +5,7 @@
 #import <React/RCTRootView.h>
 
 #define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Step One"
+#define TEXT_TO_LOOK_FOR @"React Native"
 
 @interface TodoMVCTests : XCTestCase
 
